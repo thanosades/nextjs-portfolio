4b4980fda7a62d670b3e86dd6eb9e18b4f1bfa73
Thanos Dimitriades <thanosades@gmail.com>
2021-10-29 14:17:50 +0300

    add github logo

--- components/Projects.js
+++ components/Projects.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Github from './Github';
 
 export default function Projects({ repositories }) {
   if (!repositories) {
@@ -6,30 +7,43 @@ export default function Projects({ repositories }) {
   }
 
   return (
-    <section className="grid md:grid-cols-3 sm:grid-cols-1 gap-5 max-w-4xl">
-      {repositories.map(project => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
+    <section>
+      <h2 className="text-4xl md:text-6xl text-center pb-6">My Projects</h2>
+      <hr />
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 max-w-4xl mt-8">
+        {repositories.map(project => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </section>
     </section>
   );
 }
 
 function ProjectCard({ project }) {
-  const { name, description, url } = project;
+  let { name, description, url } = project;
+
+  name = name.replace('-', ' ');
 
   return (
-    <div className="w-full block p-4 lg:p-2 transition duration-500 ease-in-out hover:shadow-2xl">
-        <Image
-          src="/img/nocturne.jpg"
-          alt="nocturne"
-          height="500"
-          width="500"
-        />
-        <p className="text-base lg:text-lg sm:text-sm">{name}</p>
-        <p className="text-base lg:text-lg sm:text-sm">{description}</p>
-        <p className="text-base lg:text-lg sm:text-sm">
-          <a target="_blank" href={url} rel="noopener noreferrer">link to repo</a>
-        </p>
+    <div className="container flex justify-between flex-col rounded-md w-full block p-4 sm:p-2 transition duration-500 border-2 border-gray-200 hover:border-gray-400 ease-in-out shadow-lg">
+      <div>
+      <Image
+        src="/img/nocturne.jpg"
+        alt="nocturne"
+        layout="responsive"
+        height="100%"
+        width="100%"
+        onClick={() => console.log('clicked on image')}
+      />
+      </div>
+      <h3 className="text-center text-4xl md:text-2xl capitalize">{name}</h3>
+      <p className="text-sm mt-2 lg:text-base">{description}</p>
+      <p className="flex mt-6 justify-between text-base lg:text-lg sm:text-sm">
+        <a target="_blank" href={url} rel="noopener noreferrer">
+        <Github />
+        </a>
+        <a target="_blank" href={url} rel="noopener noreferrer">link to live</a>
+      </p>
     </div>
   );
 }

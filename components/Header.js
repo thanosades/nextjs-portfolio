2f79b0482a69d32592fd4040562e9f7635deedcd
Thanos Dimitriades <thanosades@gmail.com>
2021-11-11 16:11:05 +0200

    fix offset

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     <header className="sticky top-0 z-10 bg-gray-800 text-white font-bold">
       <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <ul className="relative flex items-center justify-between h-16">
-          <li><a>
+          <li><a href="./">
             <div className="bg-black border-2  rounded-full p-1">TD</div>
           </a></li>
           <li><a href="#mission">Mission</a></li>

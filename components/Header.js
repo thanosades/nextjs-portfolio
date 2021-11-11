3259bdab6e6f43cf537052d391437d10afeb3a1a
Thanos Dimitriades <thanosades@gmail.com>
2021-11-11 21:41:58 +0200

    fix scrolling and add nav shadow

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 bg-green-700 text-white font-bold">
-      <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 shadow-2xl">
         <ul className="text-xl relative flex items-center justify-between h-16 overflow-hidden">
           <li><a href="./">
             <div className="bg-black border-2 rounded-full p-2">TD</div>
--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects({ projects }) {
   }
 
   return (
-    <section id="projects" className="p-10 mt-10">
+    <section id="projects" className="projects p-10 mt-10">
       <h3 className="text-5xl text-center pb-6">My Projects</h3>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 max-w-4xl mt-8">
         {projects.map(project => (

d7b67f16b4bd4698dad32b2d3bebd1af395f2738
Thanos Dimitriades <thanosades@gmail.com>
2021-11-11 16:09:22 +0200

    scrolling offset

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,14 @@
-import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 bg-gray-800 text-white font-bold">
       <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <ul className="relative flex items-center justify-between h-16">
-          <li><Link href="/" passHref={true}>
+          <li><a>
             <div className="bg-black border-2  rounded-full p-1">TD</div>
-          </Link></li>
-          <li><Link href="#mission">Mission</Link></li>
-          <li><Link href="#projects">Projects</Link></li>
+          </a></li>
+          <li><a href="#mission">Mission</a></li>
+          <li><a href="#projects">Projects</a></li>
         </ul>
       </nav>
     </header>

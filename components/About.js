088cd145596728329c5708f8d5114a19ac7763f1
Thanos Dimitriades <thanosades@gmail.com>
2021-11-13 00:50:53 +0200

    change about

--- components/About.js
+++ components/About.js
@@ -5,8 +5,7 @@ export default function About() {
     <article id="about" className="about w-screen">
       <section className="w-screen text-xl flex flex-col justify-start items-center">
         <h3 className="text-5xl">About Me</h3>
-        <p className="mt-5 w-1/2">I&apos;m a web developer with an appetite for problem solving and challenge. My goal is to write clean code and ship performant quality software.
-        My favorite languages are TypeScript and C#.</p>
+        <p className="mt-5 w-1/2">I&apos;m a web developer with an appetite for problem solving, constant growth and tackle challenges.</p>
       </section>
       <section className=" mt-10 w-screen flex flex-col justify-start items-center">
         <h3 className="text-5xl">My Skills</h3>

da10603fda60b9ee81e65081e2ea9fbe2b97ea57
Thanos Dimitriades <thanosades@gmail.com>
2022-01-05 22:20:20 +0200

    edit about me

--- components/About.js
+++ components/About.js
@@ -5,12 +5,12 @@ export default function About() {
         <h3 className="text-5xl">About Me (aka. elevator pitch)</h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
           <p>
-            Last year I lost my job due to covid. During lockdown, a friend gifted me a programming game.</p>
+            Last year I lost my job due to covid. While in lockdown, a friend gifted me a programming game.</p>
           <p>It was love at first line.</p>
           <p>
-            Now I&#39;m a software developer who&#39;s passionate about coding and addicted to learning.
-            I enjoy working with JavaScript, TypeScript, React and Node. I&#39;m also currently learning Java following best practices for
-            proper software construction, and I&#39;m planning to pick up Spring Boot next.
+            Now I'm addicted to learning and passionate about coding.
+            Aside the obvious skills listed below I&#39;m currently learning Java, following best practices for
+            good software construction (OOP, SOLID Principles and Design Patterns), and learn Spring afterwards.
           </p>
         </div>
       </section>

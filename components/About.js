6a1f64361c27b6bd784fd4e829d795dc02d4cbd7
Thanos Dimitriades <thanosades@gmail.com>
2022-01-05 21:45:19 +0200

    edit readme

--- components/About.js
+++ components/About.js
@@ -5,7 +5,8 @@ export default function About() {
         <h3 className="text-5xl">About Me</h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
           <p>
-            I'm a software developer who's passionate about coding. I enjoy working with TypeScript, React and Java.
+            I'm a software developer who's passionate about coding and addicted to learning.
+            I enjoy working with JavaScript, TypeScript, React. I'm also currently learning Java and I'm planning to pick up Spring.
           </p>
         </div>
       </section>

ac679d4f080c8888cfbe0a9575720c453ecc969b
Thanos Dimitriades <thanosades@gmail.com>
2022-01-05 22:12:12 +0200

    fix: escape apostrophes

--- components/About.js
+++ components/About.js
@@ -5,8 +5,12 @@ export default function About() {
         <h3 className="text-5xl">About Me (aka. elevator pitch)</h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
           <p>
-            I'm a software developer who's passionate about coding and addicted to learning.
-            I enjoy working with JavaScript, TypeScript, React. I'm also currently learning Java and I'm planning to pick up Spring.
+            Last year I lost my job due to covid. During lockdown, a friend gifted me a programming game. It was love at first line.
+          </p>
+          <p>
+            Now I&#39;m a software developer who&#39;s passionate about coding and addicted to learning.
+            I enjoy working with JavaScript, TypeScript, React and Node. I&#39;m also currently learning Java following best practices for
+            proper software construction, and I&#39;m planning to pick up Spring Boot next.
           </p>
         </div>
       </section>

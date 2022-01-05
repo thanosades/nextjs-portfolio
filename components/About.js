9be20f318385cb926827f31e95f5ba35dc9ebcb5
Thanos Dimitriades <thanosades@gmail.com>
2022-01-05 21:46:24 +0200

    change hero headline

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <article id="about" className="about w-screen">
       <section className="w-screen text-center text-xl flex flex-col justify-start items-center">
-        <h3 className="text-5xl">About Me</h3>
+        <h3 className="text-5xl">About Me (aka. elevator pitch)</h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
           <p>
             I'm a software developer who's passionate about coding and addicted to learning.
--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ export default function Hero({ scrollToAbout }) {
     <div id="home" className="h-screen w-screen bg-hero-pattern bg-cover bg-center">
       <div className="h-screen w-screen flex flex-col justify-center items-center gap-4 bg-black bg-opacity-50 text-white">
         <h1 className="text-5xl md:text-8xl text-center">Thanos Dimitriades</h1>
-        <h3 className="text-4xl md:text-6xl text-center">Web Developer</h3>
+        <h3 className="text-4xl md:text-6xl text-center">Software Developer</h3>
         <button onClick={scrollToAbout} className="text-xl transition-colors duration-500 ease-in-out bg-green-700 hover:bg-green-600 text-white font-bold py-3 px-6 mt-10 rounded w-auto">About Me</button>
       </div>
     </div>

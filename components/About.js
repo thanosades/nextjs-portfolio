8fc1e07abb96af6d7855b90f86bb536d596fe32a
Thanos Dimitriades <thanosades@gmail.com>
2022-01-06 02:03:02 +0200

    about me

--- components/About.js
+++ components/About.js
@@ -6,11 +6,14 @@ export default function About() {
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
           <p>
             Last year I lost my job due to covid. While in lockdown, a friend gifted me a programming game.</p>
-          <p>It was love at first line.</p>
+          <p>And it was love at first line.</p>
           <p>
-            Now I'm addicted to learning and passionate about coding.
-            Aside the obvious skills listed below I&#39;m currently learning Java, following best practices for
-            good software construction (OOP, SOLID Principles and Design Patterns), and learn Spring afterwards.
+            Now I&#39;m addicted to learning and passionate about coding, aiming for the balance between readability
+            and performance. Outside of the skills I listed below I&#39;m learning Java to expose myself in a different
+            coding paradigm, become familiar with OOP, SOLID Principles and Design Patterns, and eventually learn Spring/Spring Boot.
+          </p>
+          <p>
+            My main focus is to become part of a team of like-minded developers, grow my skillset, build good software and solve challenging problems.
           </p>
         </div>
       </section>

8063cb26545d842de9d106899e5273da843752c1
Thanos Dimitriades <thanosades@gmail.com>
2022-01-14 01:42:45 +0200

    change about section

--- components/About.js
+++ components/About.js
@@ -4,15 +4,13 @@ export default function About() {
       <section className="w-screen text-center text-xl flex flex-col justify-start items-center">
         <h3 className="text-5xl">About Me<br /> <span className="text-xl"> (aka. &#34;the elevator pitch&#34;)</span></h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
-          <p>I used to work in IT, wait tables, be a theme park cashier, and more. Found love in Zachtronics games. When I lost my job
-            during COVID lockdown I decided to learn how to program, starting from CS basics, and work my way up. I&#39;ve had enough fun to
-            take the brave decision and make it my career.
+          <p>
+            I used to work in IT. Found love in Zachtronics games (puzzle games that facilitate coding for optimization problems),
+            which I enjoyed enough to decide to learn how to program from scratch.
           </p>
-          <div>
-            <p>
-              I started as a frontend, but I&#39;m also expanding into backend territory.</p> <p>My favorite languages (so far) are TypeScript, Java and C#.
-            </p>
-          </div>
+          <p>Today I&#39;m developing software using some of the skills listed below. So far I enjoy both frontend and backend,
+            but I would prefer to stick with only one of them. Maybe one day...
+          </p>
         </div>
       </section>
     </article>

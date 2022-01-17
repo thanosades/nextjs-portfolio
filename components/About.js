6857a9fc439baaba44abba2bb8258f608ebee4e8
thanosades <thanosades@gmail.com>
2022-01-17 23:06:47 +0200

    fix grammar errors

--- components/About.js
+++ components/About.js
@@ -5,11 +5,11 @@ export default function About() {
         <h3 className="text-5xl">About Me<br /> <span className="text-xl"> (aka. &#34;the elevator pitch&#34;)</span></h3>
         <div className="text-lg flex flex-col justify-evenly mt-4 w-1/2 gap-8">
         <p>
-          I&#39;m a frontend developer who dabbles with backend.          
-          It all started when a friend gifted me a programming puzzle game. 
+          I&#39;m a frontend developer who also dabbles with backend.          
+          My journey began when a friend gifted me a programming puzzle game. 
           Now I code full-time.</p>
-          <p>My favorite combo so far are React + TypeScript + Python + Django/Flask.</p>
-          <p>I enjoy both solving problems, optimizing (not prematurely) and building software.</p>
+          <p>My favorite stack is React with TypeScript and SCSS for the frontend, and Python for the backend.</p>
+          <p>I enjoy both solving problems, optimizing for performance (as long as it&#39;s not done prematurely) and building software.</p>
         </div>
       </section>
     </article>
